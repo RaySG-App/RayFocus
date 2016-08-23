71a09011dc9aae7479a493e79c2169c861ad2332
wzy920913 <303881843@qq.com>
2016-08-23 11:27:26 +0800

    1

--- raysg/src/main/java/com/cn/honey/controller/DeviceController.java
+++ raysg/src/main/java/com/cn/honey/controller/DeviceController.java
@@ -22,7 +22,7 @@ public class DeviceController{
   {
     int result = this.deviceService.insertDevice(device);
     if (result == 1) {
-      return "success";
+      return "success1";
     }
     return "录入失败";
   }
