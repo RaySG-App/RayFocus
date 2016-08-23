7cddea0afdf9415e9dd328a6f8a073a2b3e2dd07
Administrator <Administrator@SKY-20160730PBM>
2016-08-23 12:05:00 +0800

    niao

--- raysg/src/main/java/com/cn/honey/controller/UserController.java
+++ raysg/src/main/java/com/cn/honey/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController{
   {
     int result = this.userService.insertUser(user);
     if (result == 1) {
-      return "录入成功1111111111111112222222222222224444444444444444444";
+      return "录入成功111111111shia ";
     }
     return "录入失败11111111111111112222222222222222444444444444444";
   }
